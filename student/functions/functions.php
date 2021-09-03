c6519eb5c8dd0dc655bac8ee2f81902df493688d
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-09-03 11:10:27 +0100

    student upload

--- student/functions/functions.php
+++ student/functions/functions.php
@@ -277,7 +277,7 @@ if (!empty($_FILES["assfile"]["name"])) {
 	} else {
 
 	// Allow certain file formats
-	if($imageFileType != "docx" && $imageFileType != "pdf" && $imageFileType != "doc") {
+	if($imageFileType != "docx" && $imageFileType != "pdf" && $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "doc") {
 		echo "Sorry, only document files are allowed.";
 		$uploadOk = 0;
 	} else {
@@ -338,7 +338,7 @@ if (!empty($_FILES["fle"]["name"])) {
 	} else {
 
 	// Allow certain file formats
-	if($imageFileType != "docx" && $imageFileType != "pdf" && $imageFileType != "doc") {
+	if($imageFileType != "docx" && $imageFileType != "pdf" && $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "doc") {
 		echo "Sorry, only document files are allowed.";
 		$uploadOk = 0;
 	} else {
